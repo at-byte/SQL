41760516c44786ed578a4a6b11edba6a9c788e86
at-byte <55209032+at-byte@users.noreply.github.com>
2020-07-20 19:43:18 -0400

    Add files via upload
    Select Top Nth record

---
+++ TopN-fromtable.sql
@@ -0,0 +1,28 @@
+Create table Employees
+(
+ ID int primary key identity,
+ FirstName nvarchar(50),
+ LastName nvarchar(50),
+ Gender nvarchar(50),
+ Salary int
+)
+GO
+
+Insert into Employees values ('Ben', 'Hoskins', 'Male', 70000)
+Insert into Employees values ('Mark', 'Hastings', 'Male', 60000)
+Insert into Employees values ('Steve', 'Pound', 'Male', 45000)
+Insert into Employees values ('Ben', 'Hoskins', 'Male', 70000)
+Insert into Employees values ('Philip', 'Hastings', 'Male', 45000)
+Insert into Employees values ('Mary', 'Lambeth', 'Female', 30000)
+Insert into Employees values ('Valarie', 'Vikings', 'Female', 35000)
+Insert into Employees values ('John', 'Stanmore', 'Male', 80000)
+GO
+
+select * from Employees
+
+select * from Employees where salary< (select Max(salary) from Employees)
+
+select top 1 * from Employees e inner join
+(select DENSE_RANK() over  (order by Salary desc) as RANKOR, ID,salary  from Employees ) temp
+on e.ID=temp.ID
+and Rankor=2
